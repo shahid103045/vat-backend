70fb3175c98db35d8af87cdb7b85b8dca6f57d47
shahid103045 <shahid92768@gmail.com>
2026-06-11 14:43:02 +0500

    Update check-vat.js

--- api/check-vat.js
+++ api/check-vat.js
@@ -1,7 +1,3 @@
 export default function handler(req, res) {
-  if (req.url === '/api') {
-    res.status(200).json({message: "I am blue HGGPS VAT backend is working"})
-  } else {
-    res.status(404).json({error: "Route not found"})
-  }
+  res.status(200).json({ message: "I am blue HGGPS VAT backend is working" });
 }
